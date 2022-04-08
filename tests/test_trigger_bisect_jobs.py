a9db8bffd51dbf78063af4a8c71dc28aabbcf2fb
Tina Müller <tina.mueller@suse.com>
2022-04-08 14:15:39 +0200

    Write comment after creating bisect investigate jobs
    Issue: https://progress.opensuse.org/issues/107014

--- tests/test_trigger_bisect_jobs.py
+++ tests/test_trigger_bisect_jobs.py
@@ -12,7 +12,7 @@ import json
 
 from argparse import Namespace
 from unittest.mock import call, MagicMock
-from urllib.parse import urljoin, urlparse
+from urllib.parse import urljoin, urlparse, urlunparse
 
 rootpath = os.path.abspath(os.path.join(os.path.dirname(__file__), ".."))
 
@@ -43,23 +43,30 @@ def mocked_fetch_url(url, request_type='text'):
     return content
 
 def mocked_call(cmds, dry_run=False):
-    return 1
+    return cmds
 
-def mocked_openqa_clone(cmds, dry_run, default_opts=['--skip-chained-deps', '--within-instance'], default_cmds=['_GROUP=0']):
-    return 1
+orig_fetch_url = openqa.fetch_url
 
 cmds = ['https://openqa.opensuse.org/tests/7848818', 'OS_TEST_ISSUES=21770,21926,21954,22030,22077,22085,22192', 'TEST=foo:investigate:bisect_without_21637', 'OPENQA_INVESTIGATE_ORIGIN=https://openqa.opensuse.org/tests/7848818']
 
-def test_call():
+def test_clone():
     openqa.call = MagicMock(side_effect=mocked_call)
     openqa.openqa_clone(cmds, dry_run=False)
     args = ['openqa-clone-job', '--skip-chained-deps', '--within-instance', 'https://openqa.opensuse.org/tests/7848818', 'OS_TEST_ISSUES=21770,21926,21954,22030,22077,22085,22192', 'TEST=foo:investigate:bisect_without_21637', 'OPENQA_INVESTIGATE_ORIGIN=https://openqa.opensuse.org/tests/7848818', '_GROUP=0']
     openqa.call.assert_called_once_with(args, False)
 
-def test_trigger():
+def test_comment():
+    openqa.call = MagicMock(side_effect=mocked_call)
+    openqa.openqa_comment(1234567, 'https://openqa.opensuse.org', 'foo\nbar', dry_run=False)
+    args = ['openqa-cli', 'api', '--header', 'User-Agent: openqa-trigger-bisect-jobs (https://github.com/os-autoinst/scripts)', '--host', 'https://openqa.opensuse.org', '-X', 'POST', 'jobs/1234567/comments', 'text=foo\nbar']
+    openqa.call.assert_called_once_with(args, False)
+
+def test_triggers():
     args = args_factory()
     args.url = 'https://openqa.opensuse.org/tests/7848818'
-    openqa.openqa_clone = MagicMock(return_value=1, side_effect=mocked_openqa_clone)
+    out = 'Created job #234567: opensuse-foo -> https://openqa.opensuse.org/t234567'
+    openqa.openqa_clone = MagicMock(return_value=out)
+    openqa.openqa_comment = MagicMock(return_value=out)
     openqa.fetch_url = MagicMock(side_effect=mocked_fetch_url)
     openqa.main(args)
     calls = [
@@ -68,12 +75,17 @@ def test_trigger():
         call(['https://openqa.opensuse.org/tests/7848818', 'OS_TEST_ISSUES=21637,21770,21926,21954,22030,22077,22085', 'TEST=foo:investigate:bisect_without_22192', 'OPENQA_INVESTIGATE_ORIGIN=https://openqa.opensuse.org/tests/7848818'], False),
     ]
     openqa.openqa_clone.assert_has_calls(calls)
+    comment = """Automatic bisect jobs:
 
-orig_fetch_url = openqa.fetch_url
+* **foo:investigate:bisect_without_21637**: https://openqa.opensuse.org/t234567
+* **foo:investigate:bisect_without_22085**: https://openqa.opensuse.org/t234567
+* **foo:investigate:bisect_without_22192**: https://openqa.opensuse.org/t234567
+"""
+    openqa.openqa_comment.assert_called_once_with(7848818, 'https://openqa.opensuse.org', comment, False)
 
 def test_problems():
     args = args_factory()
-    openqa.openqa_clone = MagicMock(return_value=1, side_effect=mocked_openqa_clone)
+    openqa.openqa_clone = MagicMock(return_value='')
     openqa.fetch_url = MagicMock(side_effect=mocked_fetch_url)
 
     args.url = 'http://openqa.opensuse.org/tests/123'
@@ -93,7 +105,7 @@ def test_problems():
 
 def test_directly_chained():
     args = args_factory()
-    openqa.openqa_clone = MagicMock(return_value=1, side_effect=mocked_openqa_clone)
+    openqa.openqa_clone = MagicMock(return_value='')
     openqa.fetch_url = MagicMock(side_effect=mocked_fetch_url)
 
     args.url = 'http://openqa.opensuse.org/tests/123456'
@@ -103,7 +115,7 @@ def test_directly_chained():
 def test_network_problems():
     args = args_factory()
     args.url = 'http://doesnotexist.openqa.opensuse.org/tests/12345'
-    openqa.openqa_clone = MagicMock(return_value=1, side_effect=mocked_openqa_clone)
+    openqa.openqa_clone = MagicMock(return_value='')
     openqa.fetch_url = MagicMock(side_effect=orig_fetch_url)
     try:
         openqa.main(args)
