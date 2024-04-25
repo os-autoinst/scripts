c13d05a54881c5d15f983e249a2ab08eeeefb2e6
nicksinger <nsinger@suse.de>
2024-04-25 14:11:09 +0200

    Use python3 version for racktables check script explicitly

--- racktables/find_pingable_machines.sh
+++ racktables/find_pingable_machines.sh
@@ -2,5 +2,5 @@
 found_machines=0
 while read -r fqdn; do
     ping -c1 -W1 "${fqdn}" &> /dev/null && echo "${fqdn} up" && found_machines=$((found_machines + 1))
-done <<< $(python get_unused_machines.py)
+done <<< $(python3 get_unused_machines.py)
 exit $((found_machines > 0))
