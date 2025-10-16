72b616e0a1f6a9061ff593b8a2946775e0bb23da
Oliver Kurz <okurz@suse.de>
2025-10-16 13:17:11 +0200

    racktables: Fail on unexpected errors
    Previously any error in the execution of the Python script did not cause
    the outer bash script to fail hence a longer-standing AttributeError was
    not found.
    
    Related progress issue: https://progress.opensuse.org/issues/190740

--- racktables/find_pingable_machines.sh
+++ racktables/find_pingable_machines.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
+set -euo pipefail
 found_machines=0
+output=$(python3 get_unused_machines.py)
 while read -r fqdn; do
     ping -c1 -W1 "${fqdn}" &> /dev/null && echo "${fqdn} up" && found_machines=$((found_machines + 1))
-done <<< $(python3 get_unused_machines.py)
+done <<< "$output"
 exit $((found_machines > 0))
